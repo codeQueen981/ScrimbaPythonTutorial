b2803feb1af53e97256b1737a1c7df58e94e56bf
DivaQueen <155710575+codeQueen981@users.noreply.github.com>
2026-04-20 19:56:59 +0800

    Update lesson_05_datatypes_typecasting.py

--- lesson_05_datatypes_typecasting.py
+++ lesson_05_datatypes_typecasting.py
@@ -1,1 +1,42 @@
+#Lesson 5: Datatypes & Typecasting
+  #Datatypes - Data types specify the kind of value a variable holds.
+  #Common Data Types: 
+"""
+1. Integer (int)
+Whole numbers
+x = 10
 
+2. Float (float)
+Decimal numbers
+y = 3.14
+
+3. String (str)
+Text (inside quotes)
+name = "John"
+
+4. Boolean (bool)
+True or False
+is_student = True
+
+5. List
+Collection of values
+nums = [1, 2, 3]
+
+"""
+#Check Data Type: print(type(x))
+  # Typecasting - Typecasting is the process of converting one data type into another to avoid errors when performing operations on different data types.
+"""
+Common Conversions:
+➤ String to Integer
+x = "10"
+x = int(x)
+
+➤ String to Float
+x = "3.5"
+x = float(x)
+
+➤ Number to String
+x = 100
+x = str(x)
+
+"""
