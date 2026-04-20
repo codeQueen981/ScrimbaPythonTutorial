e09b6e1098cb8a03a15521bc29be12e339d8d87b
DivaQueen <155710575+codeQueen981@users.noreply.github.com>
2026-04-20 19:16:21 +0800

    Update lesson_03_print_programflow.py

--- lesson_03_print_programflow.py
+++ lesson_03_print_programflow.py
@@ -1,1 +1,19 @@
+#For this lesson, we will be tackling the PRINT STATEMENT.
+print('Welcome to Python 101!2')
+print('Welcome to Python 101!')
 
+# 2 different segments
+print("Create nails")
+print("Create hammer")
+
+#INSTRUCTIONS: Write a nail in print statement and modify the nail in use statement with the hammer.
+# Combined into one
+print("Use hammer and nails")
+
+#TRY IT OUT!
+# 2 different segments
+print('My name is Jasmine')
+print('In college, I will be taking the course called Computer Science.')
+
+# Combined into one
+print('Her name is Jasmine, and she will be taking the course Computer Science.')
