5ecbf16848c277cd5dc709c8d46c0ab15fb3ed9c
DivaQueen <155710575+codeQueen981@users.noreply.github.com>
2026-04-20 19:40:33 +0800

    Update lesson_04_variables.py

--- lesson_04_variables.py
+++ lesson_04_variables.py
@@ -1,1 +1,48 @@
+#Lesson 4: Variables
+# Variables - a named container used to store data that can be used and changed in a program.
+# A variable is like a box with a label:
+
+failed_subjects="2"
+name='John'
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + '  will need to redo ' + failed_subjects + '  courses.')
+name="Eric"
+print(name + '  is doing well in geography.')
+
+
+#TRY IT OUT!
+#write your sentences/inputes first
+print('Dear Ms Jasmine,')
+print('Sassy has failed 2 subjects')
+print('First, is Chemistry which is a flat 80')
+print('Second, is Basic Calculus which is 84.')
+
+#add on the variables
+failed_subjects= "2" 
+chemistry= "80"
+BasicCal= "84"
+name= "Jasmine"
+name_2= "Sassy"
+
+#These are your variables & you will be using this format to input it in the code within these sentences below.
+' + name + '
+' + name_2 + '
+' + failed_subjects + '
+' + BasicCal + '
+' + chemistry + '
+
+print('Dear Ms ' + name + ',')
+print('Sassy has failed ' + failed_subjects + ' subjects.')
+print('First, is Chemistry which is a flat ' + chemistry + '.')
+print('Second, is Basic Calculus which is ' + BasicCal + '.')
+
+
+
+
+
+
+
+
+
 
